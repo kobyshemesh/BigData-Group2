ce2c20b93c074a003d7c9277762adde6875a632b
Yanir Calisar <ycalisar@gmail.com>
2017-12-09 23:54:27 +0200

    Converted the project to JAVA - Maven

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class Main {
+
+    private static Log log = LogFactory.getLog(Main.class);
+
+    public static void main(String[] args){
+
+        log.info("123");
+
+    }
+
+}
