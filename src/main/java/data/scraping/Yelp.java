98ed954ce901a358a692bf75245e426013eb0bd1
Yanir Calisar <ycalisar@gmail.com>
2017-12-10 00:01:22 +0200

    added yelp scrapping class

---
+++ src/main/java/data/scraping/Yelp.java
@@ -0,0 +1,16 @@
+package data.scraping;
+
+public class Yelp {
+
+    /*
+
+
+    https://api.yelp.com/v3/businesses/search
+        ?radius=1000
+        &latitude=51.5100910
+        &longitude=-0.1345680
+
+     */
+
+
+}
