ccf50c242cc8a275aa3ff6de0fd94da42b848b0d
Yanir Calisar <ycalisar@gmail.com>
2017-12-11 02:04:42 +0200

    Create r.scripts directories Installed R4Intellij

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,27 @@
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+//import org.apache.commons.logging.Log;
+//import org.apache.commons.logging.LogFactory;
+//
+
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class Main {
 
-    private static Log log = LogFactory.getLog(Main.class);
+//    private static Log log = LogFactory.getLog(Main.class);
+
+    private static Connection conn;
 
     public static void main(String[] args){
 
-        log.info("123");
+        try {
+            PreparedStatement ps = conn.prepareCall("SELECT * FROM hotel_reviews LIMIT 1");
+            ps.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
 
     }
 
